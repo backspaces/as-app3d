64e9b1afd5b27e5a08b2df4bc160292d6c8c0bf9
backspaces <owen@backspaces.net>
2017-11-28 15:55:17 +0100

    Minor changes: Hello, Package.json - Hello: reformat. Add links - Package.json: move CoreAS from dependencies to devDependencies

--- docs/models/hello.js
+++ docs/models/hello.js
@@ -4,6 +4,7 @@ import util from '../src/core/util.js'
 
 class Hello extends Model {
   // Inherit default constructor.
+
   setup () {
     this.patches.ask(p => {
       p.color = ColorMap.LightGray.randomColor()
@@ -14,9 +15,17 @@ class Hello extends Model {
     this.turtles.create(10, t => {
       const patch = this.patches.oneOf()
       t.setxy(patch.x, patch.y)
+      t.size = 2
       // If we don't set color, a random color will be used
     })
+
+    this.turtles.ask(t => {
+      this.links.create(t, this.turtles.otherOneOf(t), (link) => {
+        link.color = this.randomColor() // Uses Model's colormap
+      })
+    })
   }
+
   step () {
     this.turtles.ask(t => {
       t.direction += util.randomCentered(0.1)
@@ -28,4 +37,8 @@ class Hello extends Model {
 const model = new Hello()
 model.setup()
 model.start()
-util.toWindow({ ColorMap, Model, util, model })
+
+// Debugging
+util.toWindow({ ColorMap, Model, util })
+const { patches, turtles, links } = model
+util.toWindow({ patches, turtles, links, model })
